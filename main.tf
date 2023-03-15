4396db9623df152209735524e5e9826e253f2827
ericktennesy <122739624+ericktennesy@users.noreply.github.com>
2023-03-15 13:26:35 +0000

    Security Groups

--- main.tf
+++ main.tf
@@ -26,14 +26,14 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 
-  aws_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 }
 
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Resource group for testing environment"
 
-  vpc_id = data_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
