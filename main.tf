cd0b7253718569a4b476993b9e549fe3da8c48cf
Ernest Mikolaiev <ericktennesy@gmail.com>
2023-03-23 13:48:55 +0000

    Pizaddadada

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
 data "aws_ami" "app_ami" {
   most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
 }
 
 data "aws_vpc" "default" {
@@ -11,6 +23,5 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   tags = {
-    Name = "TestStart"
+    Name = "ScalingTest"
   }
-}
