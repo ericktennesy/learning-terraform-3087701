c5bd87934c39195fdc3429d25db3086cffdf3d19
ericktennesy <122739624+ericktennesy@users.noreply.github.com>
2023-03-23 12:54:11 +0000

    Test Delete

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
 data "aws_ami" "app_ami" {
   most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
 }
 
 data "aws_vpc" "default" {
@@ -25,43 +13,4 @@ resource "aws_instance" "blog" {
   tags = {
     Name = "ScalingTest"
   }
-
-  vpc_security_group_ids = [aws_security_group.blog.id]
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Resource group for testing environment"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_all_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = -1
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
